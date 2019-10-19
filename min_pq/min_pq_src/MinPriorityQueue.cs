b43b392928596638ef90d7ac58e86da633dd4cf8
Eric Chen <ericc661@gmail.com>
2019-10-18 23:29:09 -0400

    getting started, gitignore

---
+++ min_pq/min_pq_src/MinPriorityQueue.cs
@@ -0,0 +1,24 @@
+﻿/* Implementation of a simple minimum priority queue
+ */
+
+using System;
+using System.Diagnostics;
+
+namespace min_pq
+{
+    public class MinPriorityQueue
+    {
+        /* summary: given state of underlying heap, check each non-root node's
+         *   parent priority is less than its priority
+         */
+        private void CheckInvariant()
+        {
+            throw new NotImplementedException();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("here's some stuff");
+        }
+    }
+}
