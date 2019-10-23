95ba63d634855874f9131383270aac87d27bcb1d
Eric Chen <ericc661@gmail.com>
2019-10-23 11:09:50 -0400

    added unit test project, considering making another class containing heap utility

--- min_pq/min_pq_src/MinPriorityQueue.cs
+++ min_pq/min_pq_src/MinPriorityQueue.cs
@@ -1,4 +1,4 @@
-﻿/* Implementation of a simple minimum priority queue
+﻿/* Implementation of a simple minimum priority queue using a heap
  */
 
 using System;
@@ -8,17 +8,41 @@ namespace min_pq
 {
     public class MinPriorityQueue
     {
+        private int[] heap;
+        private int size; // actual # elements in heap, differs from capacity
+        
         /* summary: given state of underlying heap, check each non-root node's
          *   parent priority is less than its priority
+         * requires: heap to be the underlying array representing the heap,
+         *   size to be the number of elements currently in the heap
+         * effects: checks if array/heap preserves above property as well as
+         *   ensures size is less than or equal to capacity. Throws an exception
+         *   if either of these are not met.
          */
-        private void CheckInvariant()
+        public void CheckInvariants(int[] heap, int size)
         {
+            /*if (size > capacity)
+            {
+                throw new Exception("Heap size exceeds capacity");
+            }
+            else
+            {
+                //for (int i=0; i<this.size; )
+            }*/
             throw new NotImplementedException();
+        }
+
+        // constructor
+        public MinPriorityQueue(int capacity)
+        {
+            this.heap = new int[capacity];
+            this.size = 0;
         }
 
         static void Main(string[] args)
         {
-            Console.WriteLine("here's some stuff");
+            MinPriorityQueue pq = new MinPriorityQueue(100);
+            //Console.WriteLine(pq[0]);
         }
     }
 }
---
+++ min_pq/min_pq_test/MinPQTest.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace min_pq
+{
+    [TestClass]
+    public class MinPQTest
+    {
+        private int[] heap1;
+        private int size1;
+
+        [TestInitialize]
+        public void InitTestState()
+        {
+            heap1 = new int[100];
+            MinPriorityQueue pq = new MinPriorityQueue(100);
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
